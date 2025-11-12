c2c090b77fad1a7319739431c785c72aa83a6488
Stas Bekman <stas00@users.noreply.github.com>
2025-11-12 11:13:00 -0800

    see_mem_usage: make always work (#7688)
    `logger` is the wrong facility for `see_mem_usage` messages, since it
    should always print (if `force=True`) and the current implementation
    doesn't guarantee that leading to wasted dev time puzzling over it. So
    fixing to use `print` instead.

--- deepspeed/runtime/utils.py
+++ deepspeed/runtime/utils.py
@@ -783,15 +783,15 @@ def see_memory_usage(message, force=False):
     gc.collect()
 
     # Print message except when distributed but not rank 0
-    logger.info(message)
-    logger.info(f"MA {round(get_accelerator().memory_allocated() / (1024 * 1024 * 1024),2 )} GB \
+    print(message)
+    print(f"MA {round(get_accelerator().memory_allocated() / (1024 * 1024 * 1024),2 )} GB \
         Max_MA {round(get_accelerator().max_memory_allocated() / (1024 * 1024 * 1024),2)} GB \
         CA {round(torch_memory_reserved() / (1024 * 1024 * 1024),2)} GB \
         Max_CA {round(torch_max_memory_reserved() / (1024 * 1024 * 1024))} GB ")
 
     vm_stats = psutil.virtual_memory()
     used_GB = round(((vm_stats.total - vm_stats.available) / (1024**3)), 2)
-    logger.info(f'CPU Virtual Memory:  used = {used_GB} GB, percent = {vm_stats.percent}%')
+    print(f'CPU Virtual Memory:  used = {used_GB} GB, percent = {vm_stats.percent}%')
 
     # get the peak memory to report correct data, so reset the counter for the next call
     get_accelerator().reset_peak_memory_stats()
